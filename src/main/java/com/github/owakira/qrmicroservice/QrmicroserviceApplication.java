1e0c5875c905900f6d8f070d7095d937d98850ec
Damir Mirasov <damirmirasovmain@gmail.com>
2023-04-16 19:36:19 +0600

    Initial commit

---
+++ src/main/java/com/github/owakira/qrmicroservice/QrmicroserviceApplication.java
@@ -0,0 +1,11 @@
+package com.github.owakira.qrmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrmicroserviceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(QrmicroserviceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/github/owakira/qrmicroservice/QrmicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.owakira.qrmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrmicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
